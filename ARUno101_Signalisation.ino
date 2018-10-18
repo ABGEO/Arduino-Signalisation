cd0fd282e6e19fc3c215cb6a7917710969c0c442
Temuri Takalandze(ABGEO) <takalandzet@gmail.com>
2018-10-18 23:52:48 +0400

    Added Signalisation Function

--- ARUno101_Signalisation.ino
+++ ARUno101_Signalisation.ino
@@ -4,9 +4,14 @@
 #define SS_PIN 10
 #define RST_PIN 9
 
-#define BOOZER_PIN 8
-#define RED_LED_PIN 7
-#define GREEN_LED_PIN 6
+#define PHOTO_CELL_PIN A0
+#define LASER_PIN A1
+#define GREEN_LED_PIN A2
+#define RED_LED_PIN A3
+#define SIGNALISATION_LED_PIN A4
+#define BOOZER_PIN A5
+
+int PhotoCellValue;
 
 bool signalisation_enabled = true;
 bool signalisation_active = false;
@@ -37,15 +42,19 @@ void boozer (int x)
 }
 
 void setup()
-{ 
+{
+  Serial.begin(9600);
   SPI.begin(); 
   rfid.init();
   
   pinMode(BOOZER_PIN, OUTPUT);
   pinMode(RED_LED_PIN, OUTPUT);
   pinMode(GREEN_LED_PIN, OUTPUT);
+  pinMode(LASER_PIN, OUTPUT);
+  pinMode(SIGNALISATION_LED_PIN, OUTPUT);
   
   digitalWrite(RED_LED_PIN, HIGH);
+  digitalWrite(LASER_PIN, HIGH);
 }
 
 void loop()
@@ -66,19 +75,41 @@ void loop()
         {
           digitalWrite(RED_LED_PIN, HIGH);
           digitalWrite(GREEN_LED_PIN, LOW);
+          digitalWrite(LASER_PIN, HIGH);
         }
         else
         {
           digitalWrite(RED_LED_PIN, LOW);
           digitalWrite(GREEN_LED_PIN, HIGH);
+          digitalWrite(LASER_PIN, LOW);
         }
       }
       else
       {
+        digitalWrite(SIGNALISATION_LED_PIN, HIGH);
         boozer(2);
         delay(300);
+        digitalWrite(SIGNALISATION_LED_PIN, LOW);
       }
   }
-
   rfid.halt();
+  
+  PhotoCellValue = analogRead(PHOTO_CELL_PIN);
+  
+  Serial.println(PhotoCellValue);
+  
+  if (signalisation_enabled && (PhotoCellValue < 800))
+    signalisation_active = true;
+
+  if (signalisation_active)
+  {
+    digitalWrite(SIGNALISATION_LED_PIN, HIGH);
+    tone(BOOZER_PIN, 800);
+    digitalWrite(LASER_PIN, LOW);
+  }
+  else
+  {
+    digitalWrite(SIGNALISATION_LED_PIN, LOW);
+    noTone(BOOZER_PIN);
+  }
 }
